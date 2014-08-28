ec2fe5c9d7aa78d9baa50f7d89a7024d35de4d8b
Nicole Bouchard <nbouchard@verticalresponse.com>
2014-08-28 15:21:16 -0700

    comparision graph styling changes

--- assets/js/graphs/comparison_graph.js
+++ assets/js/graphs/comparison_graph.js
@@ -1,13 +1,35 @@
 var comparisonGraph = c3.generate({
     bindto: '#comparisonGraph',
+    color: { pattern: ['#e421f9', '#23b0f7' ] },
     data: {
+        names: {
+            data1: 'Email 1',
+            data2: 'Email 2'
+        },
         columns: [
-            ['data1', 300, 350, 300, 0, 0, 120],
-            ['data2', 130, 100, 140, 200, 150, 50]
+            ['data1', 5, 7, 2, 3, 2, 0],
+            ['data2', 8, 5, 7, 3, 1, 2]
         ],
         types: {
             data1: 'area-spline',
             data2: 'area-spline'
         }
+    },
+    axis: {
+        x: {
+            label: {
+                text: 'per Day',
+                position: 'outer-center'
+            }
+        },
+        y: {
+            label: {
+                text: 'Opens',
+                position: 'outer-middle'
+            }
+        }
+    },
+    tooltip: {
+        grouped: false
     }
 });
