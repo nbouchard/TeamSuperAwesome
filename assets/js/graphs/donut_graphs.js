2b6b122ac872d83afd9b605ec3dcdd882cb5bf33
Nicole Bouchard <nbouchard@verticalresponse.com>
2014-08-28 14:02:35 -0700

    text elements for donut graphs

--- assets/js/graphs/donut_graphs.js
+++ assets/js/graphs/donut_graphs.js
@@ -1,24 +1,54 @@
+var tau = 2*Math.PI,
+    width = 260,
+    height = 260,
+    innerRadius = 70,
+    outerRadius = 100
+    percent = 75;
 
-var donuts = d3.selectAll(".donut_graph svg");
+var donuts = d3.selectAll(".donut_graph svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 var circle = d3.svg.arc()
-    .innerRadius(75)
-    .outerRadius(100)
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius)
     .startAngle(0)
-    .endAngle(2*Math.PI);
+    .endAngle( tau );
 
 var arc = d3.svg.arc()
-    .innerRadius(75)
-    .outerRadius(100)
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius)
     .startAngle(0)
-    .endAngle(1.5*Math.PI);
+    .endAngle( tau*(percent/100) );
 
 donuts.append("path")
     .attr("d", circle)
-    .attr("class", "circle")
-    .attr("transform", "translate(130,130)");
+    .attr("class", "circle");
 
 donuts.append("path")
     .attr("d", arc)
-    .attr("class", "arc")
-    .attr("transform", "translate(130,130)");
+    .attr("class", "arc");
+
+donuts.append("text")
+    .attr("x", 0)
+    .attr("y", -10)
+    .attr("text-anchor", "middle")
+    .style({'fill':'#444444' , 'font-size':'24px'})
+    .text( percent + "%");
+
+
+donuts.append("text")
+    .attr("x", 0)
+    .attr("y", 15)
+    .attr("text-anchor", "middle")
+    .style({'fill':'#848484' , 'font-size':'18px'})
+    .text( "Opens");
+
+donuts.append("text")
+    .attr("x", 0)
+    .attr("y", 37)
+    .attr("text-anchor", "middle")
+    .style({'fill':'rgb(126, 126, 126)' , 'font-size':'12px', 'font-family': 'Arial'})
+    .text( "25/50");
